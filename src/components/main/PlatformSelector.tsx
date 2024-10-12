26da122d03c13c154feba05344fe2a92bb014794
Christian DerManuelian <christian.dermanuelian@gmail.com>
2024-10-12 16:46:25 -0700

    Implemented Search

--- src/components/main/PlatformSelector.tsx
+++ src/components/main/PlatformSelector.tsx
@@ -1,11 +1,18 @@
 import usePlatforms from "../../hooks/usePlatforms";
-import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
+import {
+  Button,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuItem,
+  MenuList,
+} from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 import { Platform } from "../../hooks/useGames";
 
 interface Props {
   selectedPlatform: Platform | null;
-  onSelectPlatform: (platform: Platform) => void;
+  onSelectPlatform: (platform: Platform | null) => void;
 }
 
 const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
@@ -16,9 +23,13 @@ const PlatformSelector = ({ selectedPlatform, onSelectPlatform }: Props) => {
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown></BsChevronDown>}>
-        {selectedPlatform?.name || "Platforms"}
+        {selectedPlatform?.name || "Platforms: All"}
       </MenuButton>
       <MenuList>
+        <MenuItem onClick={() => onSelectPlatform(null)} key={"all_platform"}>
+          All
+        </MenuItem>
+        <MenuDivider />
         {data.map((platform) => (
           <MenuItem
             onClick={() => onSelectPlatform(platform)}
--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { createRoot } from "react-dom/client";
 import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 
 import App from "./App.tsx";
+import "./index.css";
 import theme from "./theme";
 
 createRoot(document.getElementById("root")!).render(
